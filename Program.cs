e777939968161fdec9e47d341be085ebcd5e2302
Julia <wuwara@mail.ru>
2023-01-24 23:29:47 +0300

    I wrote the first method, found the size of the array

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿ string[] array = {"jo", "hello", "77", "hej", "4" };
+
+    
+    
+int FindSize(string[] array){
+ int size = 0;
+  for(int i = 0; i< array.Length; i++){
+    if (array[i].Length < 4){
+      size++;
+    }     
+  }
+  return size;
+}
